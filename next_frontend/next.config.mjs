4b1e403d4012d195e803d526394f1dfd46d005db
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2024-02-10 02:32:00 +0530

    /

--- next_frontend/next.config.mjs
+++ next_frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '*',
+                source: '/*',
                 destination: 'https://dull-erin-donkey-garb.cyclic.app/*',
             },
         ]
